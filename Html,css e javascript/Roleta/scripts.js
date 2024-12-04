865eec8ac86d6f2afc2c41d38af75d95c58bce48
Wes-Silva2003 <silva.wes2003@gmail.com>
2024-12-04 11:54:39 -0300

    Delete Html,css e javascript directory

--- Html,css e javascript/Roleta/scripts.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector(".container");
-const btn = document.getElementById("spin");
-let angle = 0;
-
-btn.onclick = function () {
-    const spin = Math.ceil(Math.random() * 360) + 360 * 3; // Gira pelo menos 3 vezes
-    angle += spin; // Adiciona o novo giro ao ângulo acumulado
-    container.style.transform = `rotate(${angle}deg)`; // Aplica a rotação
-};
